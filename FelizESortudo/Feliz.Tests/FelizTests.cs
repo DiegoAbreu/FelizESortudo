3283740ad21b7ceca73b63cf6639348cceab2ef4
DiegoAbreu <diego.souza.abreu@gmail.com>
2017-08-12 10:05:55 -0300

    Feliz e Sortudo
    Programa Feliz e Sortudo

---
+++ FelizESortudo/Feliz.Tests/FelizTests.cs
@@ -0,0 +1,161 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feliz.Tests
+{
+    public class FelizTests
+    {
+        [TestFixture]
+        public class FelizCasosDeTestes
+        {
+            [Test]
+            public void EhFeliz7()
+            {
+                int num;
+                num = 7;
+                {
+                    var verifica = new Dictionary<int, bool>();
+                    var felicidade = num;
+
+                    while (!verifica.ContainsKey(felicidade))
+                    {
+                        verifica.Add(felicidade, true);
+                        var start = felicidade;
+                        felicidade = 0;
+                        while (start != 0)
+                        {
+                            felicidade += (start % 10) * (start % 10);
+                            start /= 10;
+                        }
+                    }
+                    Assert.That((felicidade == 1), Is.EqualTo(true));
+                }
+            }
+
+            [Test]
+            public void NaoEhFeliz21()
+            {
+                int num;
+                num = 21;
+                {
+                    var verifica = new Dictionary<int, bool>();
+                    var felicidade = num;
+
+                    while (!verifica.ContainsKey(felicidade))
+                    {
+                        verifica.Add(felicidade, true);
+                        var start = felicidade;
+                        felicidade = 0;
+                        while (start != 0)
+                        {
+                            felicidade += (start % 10) * (start % 10);
+                            start /= 10;
+                        }
+                    }
+                    Assert.That((felicidade != 1), Is.EqualTo(true));
+                }
+            }
+
+            [Test]
+            public void EhFeliz28()
+            {
+                int num;
+                num = 28;
+                {
+                    var verifica = new Dictionary<int, bool>();
+                    var felicidade = num;
+
+                    while (!verifica.ContainsKey(felicidade))
+                    {
+                        verifica.Add(felicidade, true);
+                        var start = felicidade;
+                        felicidade = 0;
+                        while (start != 0)
+                        {
+                            felicidade += (start % 10) * (start % 10);
+                            start /= 10;
+                        }
+                    }
+                    Assert.That((felicidade == 1), Is.EqualTo(true));
+                }
+            }
+
+            [Test]
+            public void NaoEhFeliz142()
+            {
+                int num;
+                num = 142;
+                {
+                    var verifica = new Dictionary<int, bool>();
+                    var felicidade = num;
+
+                    while (!verifica.ContainsKey(felicidade))
+                    {
+                        verifica.Add(felicidade, true);
+                        var start = felicidade;
+                        felicidade = 0;
+                        while (start != 0)
+                        {
+                            felicidade += (start % 10) * (start % 10);
+                            start /= 10;
+                        }
+                    }
+                    Assert.That((felicidade != 1), Is.EqualTo(true));
+                }
+            }
+
+            [Test]
+            public void NaoEhFeliz37()
+            {
+                int num;
+                num = 37;
+                {
+                    var verifica = new Dictionary<int, bool>();
+                    var felicidade = num;
+
+                    while (!verifica.ContainsKey(felicidade))
+                    {
+                        verifica.Add(felicidade, true);
+                        var start = felicidade;
+                        felicidade = 0;
+                        while (start != 0)
+                        {
+                            felicidade += (start % 10) * (start % 10);
+                            start /= 10;
+                        }
+                    }
+                    Assert.That((felicidade != 1), Is.EqualTo(true));
+                }
+            }
+
+            [Test]
+            public void EhFeliz100()
+            {
+                int num;
+                num = 100;
+                {
+                    var verifica = new Dictionary<int, bool>();
+                    var felicidade = num;
+
+                    while (!verifica.ContainsKey(felicidade))
+                    {
+                        verifica.Add(felicidade, true);
+                        var start = felicidade;
+                        felicidade = 0;
+                        while (start != 0)
+                        {
+                            felicidade += (start % 10) * (start % 10);
+                            start /= 10;
+                        }
+                    }
+                    Assert.That((felicidade == 1), Is.EqualTo(true));
+                }
+            }
+        }
+    }
+}
+
---
+++ FelizESortudo/Sortudo.Tests/SortudoTests.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortudo.Tests
+{
+    public class SortudoTests
+    {
+        [TestFixture]
+        public class SortudoCasosDeTestes
+        {
+            [Test]
+            public void EhSortudo7()
+            {
+                int num;
+                num = 7;
+                int[] sorte = new int[] { 1, 3, 7, 9, 13, 15, 21, 25, 31, 33, 37, 43, 49, 51, 63, 67, 69, 73, 75, 79, 87, 93, 99 };
+                Assert.That((sorte.Contains(num)), Is.EqualTo(true));
+            }
+
+            [Test]
+            public void EhSortudo21()
+            {
+                int num;
+                num = 21;
+                int[] sorte = new int[] { 1, 3, 7, 9, 13, 15, 21, 25, 31, 33, 37, 43, 49, 51, 63, 67, 69, 73, 75, 79, 87, 93, 99 };
+                Assert.That((sorte.Contains(num)), Is.EqualTo(true));
+            }
+
+            [Test]
+            public void NaoEhSortudo28()
+            {
+                int num;
+                num = 28;
+                int[] sorte = new int[] { 1, 3, 7, 9, 13, 15, 21, 25, 31, 33, 37, 43, 49, 51, 63, 67, 69, 73, 75, 79, 87, 93, 99 };
+                Assert.That((sorte.Contains(num)), Is.EqualTo(false));
+            }
+
+            [Test]
+            public void NaoEhSortudo142()
+            {
+                int num;
+                num = 142;
+                int[] sorte = new int[] { 1, 3, 7, 9, 13, 15, 21, 25, 31, 33, 37, 43, 49, 51, 63, 67, 69, 73, 75, 79, 87, 93, 99 };
+                Assert.That((sorte.Contains(num)), Is.EqualTo(false));
+            }
+
+            [Test]
+            public void EhSortudo37()
+            {
+                int num;
+                num = 37;
+                int[] sorte = new int[] { 1, 3, 7, 9, 13, 15, 21, 25, 31, 33, 37, 43, 49, 51, 63, 67, 69, 73, 75, 79, 87, 93, 99 };
+                Assert.That((sorte.Contains(num)), Is.EqualTo(true));
+            }
+
+            [Test]
+            public void NaoEhSortudo100()
+            {
+                int num;
+                num = 100;
+                int[] sorte = new int[] { 1, 3, 7, 9, 13, 15, 21, 25, 31, 33, 37, 43, 49, 51, 63, 67, 69, 73, 75, 79, 87, 93, 99 };
+                Assert.That((sorte.Contains(num)), Is.EqualTo(false));
+            }
+        }
+    }
+}
+
